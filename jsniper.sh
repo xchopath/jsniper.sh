cf11703250e3cb73625352f7bdcaa004bb5ec569
Novran Faathir <novran.faathir@gmail.com>
2024-01-18 14:59:57 +0700

    Update jsniper.sh

--- jsniper.sh
+++ jsniper.sh
@@ -5,7 +5,7 @@ TARGET="${1}"
 function JavascriptScan() {
 	TARGET="${1}"
 	JSFILENAME="/tmp/$(shuf -i 100000-999999 | head -1 | md5sum | awk '{print $1".js"}')";
-	curl -sk "${TARGET}" -o ${JSFILENAME};
+	curl -sk --connect-timeout 10 --max-time 10 "${TARGET}" -o ${JSFILENAME};
 	nuclei -silent -t file/js -u ${JSFILENAME}
 	nuclei -silent -t file/keys -u ${JSFILENAME}
 	nuclei -silent -t file/xss/dom-xss.yaml -u ${JSFILENAME}
