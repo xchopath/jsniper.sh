5e107c7de9bb6929b16554fbc14125e1db82b208
Novran Faathir <novran.faathir@gmail.com>
2024-01-18 13:47:46 +0700

    Create jsniper.sh

---
+++ jsniper.sh
@@ -0,0 +1,30 @@
+#!/usr/env/bin bash
+
+TARGET="${1}"
+
+function JavascriptScan() {
+	TARGET="${1}"
+	JSFILENAME="/tmp/$(shuf -i 100000-999999 | head -1 | md5sum | awk '{print $1".js"}')";
+	curl -sk "${TARGET}" -o ${JSFILENAME};
+	nuclei -silent -t file/js -u ${JSFILENAME}
+	nuclei -silent -t file/keys -u ${JSFILENAME}
+	nuclei -silent -t file/xss/dom-xss.yaml -u ${JSFILENAME}
+	rm ${JSFILENAME}
+}
+
+function JavascriptURLRecon() {
+	DOMAIN="${1}"
+	function URLRecon() {
+		DOMAIN="${1}"
+		gau ${DOMAIN} 2> /dev/null
+		echo "${DOMAIN}" | httpx -silent | katana -silent
+	}
+	URLRecon "${DOMAIN}" | grep '\.js' | sort -V | uniq
+}
+
+for JSLINK in $(JavascriptURLRecon "${TARGET}")
+do
+	echo "[+] ${JSLINK}"
+	JavascriptScan "${JSLINK}"
+	echo "------------------------------"
+done
